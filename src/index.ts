9f395931098262c36f4a0c3e9d0ddb9c1751748f
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-04-07 02:45:25 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import socketIO from 'socket.io';
+import path from 'path';
+import http from 'http';
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new socketIO.Server(httpServer);
+
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+
+io.on('connection', (socket) => {
+    console.log(`New Connection: ${socket.id}`);
+    
+    socket.on('message', message => {
+        socket.emit('received', `Mensagem Recebida! Mensagem: ${message}`);
+    })
+})
+
+httpServer.listen(3333);
